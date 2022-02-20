2671c71ab0bba1f67de22a9b2a97f483aee8db16
SergeyKomanich <komanichs@gmail.com>
2022-02-20 13:33:23 +0200

    add format for str and ord/chr

---
+++ string_data_type/formatting_strings.py
@@ -0,0 +1,11 @@
+# TASK_9.5.1
+s = 'In {0}, someone paid {1} {2} for two pizzas.'
+
+print(s.format(2010, '10k', 'Bitcoin'))
+
+# TASK_9.5.1
+year = 2010
+amount = '10K'
+currency = 'Bitcoin'
+
+print(f'In {year}, someone paid {amount} {currency} for two pizzas.')
